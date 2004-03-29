daf241f0f585aca00e0c92d5fc27a3781c7611c8
pletzer <>
2004-03-29 15:08:02 +0000

    +info

--- pypspline/senddoc.sh
+++ pypspline/senddoc.sh
@@ -5,3 +5,10 @@ tar cvfz pypspline_html.tgz *.htm *.html
 scp pypspline_html.tgz pletzer@pypspline.sf.net:/home/groups/p/py/pypspline/htdocs
 ssh pletzer@pypspline.sf.net "cd /home/groups/p/py/pypspline/htdocs; tar xvfz pypspline_html.tgz; cp index.htm index.html"
 cd ..
+
+echo "to upload a file release:"
+echo "ftp upload.sourceforge.net (anonymous)"
+echo "cd /incoming"
+echo "bin"
+echo "hash"
+echo "mput dist/pypspline*.tar.gz"
