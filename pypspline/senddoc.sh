36ff89c9ca16d8652178f2424d2d045584a17fed
pletzer <>
2004-04-19 12:27:24 +0000

    doc improvements

--- pypspline/senddoc.sh
+++ pypspline/senddoc.sh
@@ -3,13 +3,13 @@
 cd pypspline
 for f in `ls *.py`; do
     f2=`echo $f | awk -F'.' '{print $1}'`
-    pydoc -w $f2
+    pydoc2 -w $f2
 done
 rm __init__.html
 cp *.html ../html
 cd ..
 cd html
-tar cvfz pypspline_html.tgz *.htm *.html
+tar cvfz pypspline_html.tgz *.htm *.html *.py
 scp pypspline_html.tgz pletzer@pypspline.sf.net:/home/groups/p/py/pypspline/htdocs
 ssh pletzer@pypspline.sf.net "cd /home/groups/p/py/pypspline/htdocs; tar xvfz pypspline_html.tgz; cp index.htm index.html"
 cd ..
