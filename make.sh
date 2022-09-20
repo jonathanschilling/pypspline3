86ef6963f30318bf0ffb7ba15ca05d1e52700fca
Jonathan Schilling <jonathan.schilling@mail.de>
2022-09-20 21:23:30 +0200

    fixed compile setup

--- make.sh
+++ make.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 rm -rf obj/*
-rm pypspline/*pspline_wrapped*
+rm -f pypspline/*pspline_wrapped*
+rm -f f90wrap_toplevel.f90
 
 mkdir -p obj/
-
-f90wrap -m pspline_wrapped -k kind_map src/*.f90
 
 for i in src/*.f90
 do
   gfortran -c ${i} -o obj/`echo ${i} | sed -e 's/^src//g' | sed -e 's/f90$/o/g'`
 done
+
+f90wrap -m pspline_wrapped -k kind_map src/*.f90
 
 f2py -c -m _pspline_wrapped -DF2PY_REPORT_ON_ARRAY_COPY=1 f90wrap_toplevel.f90 obj/*.o
 
