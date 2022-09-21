654ecb5cddde7e9e85e87f6427e3bca2104bf404
Jonathan Schilling <jonathan.schilling@mail.de>
2022-09-21 16:55:55 +0200

    3D tests now working as well

--- pypspline/test_interp_3d.py
+++ pypspline/test_interp_3d.py
@@ -5,7 +5,9 @@
 import numpy as _np
 from pypspline.pspline_3d import pspline, griddata
 
-EPS = 1.e-10
+import pytest
+
+eps = _np.finfo(float).eps
 
 def pointVal(spl, x1, x2, x3, ff):
     error = 0
@@ -282,144 +284,238 @@ def arrayDxz(spl, x1, x2, x3, ff):
 
 ##################################################################
 
+# original grid
+n1, n2, n3 = 11, 21, 31
+x1 = _np.linspace(0., 1., n1)
+x2 = _np.linspace(0., 1., n2)
+x3 = _np.linspace(0., 1., n3)
+spl = pspline(x1, x2, x3)
+xx1, xx2, xx3 = griddata(x1, x2, x3)
+ff = xx1**3 + 2*xx2**3 + 3*xx3**3 + xx1**3 * xx2**2 * xx3
+spl.setup(ff)
+
+# new grid
+x1 = _np.linspace(0., 1., n1-1)
+x2 = _np.linspace(0., 1., n2-1)
+x3 = _np.linspace(0., 1., n3-1)
+xx1, xx2, xx3 = griddata(x1, x2, x3)
+ff = xx1**3 + 2*xx2**3 + 3*xx3**3 + xx1**3 * xx2**2 * xx3
+fx = 3*xx1**2 + 3*xx1**2 * xx2**2 * xx3
+fy = 6*xx2**2 + 2*xx1**3 * xx2 * xx3
+fz = 9*xx3**2 +   xx1**3 * xx2**2
+fxx = 6*xx1 +  6*xx1 * xx2**2 * xx3
+fxy = 6 * xx1**2 * xx2 * xx3
+fyy = 12*xx2 + 2*xx1**3 * xx3
+fxz = 3*xx1**2 * xx2**2
+fzz = 18*xx3
+fyz = 2*xx1**3 * xx2
+
+
+def test_pointVal():
+    error = pointVal(spl, x1, x2, x3, ff)
+    print('error = %g'%error)
+    assert error < 10*10*eps
+
+def test_cloudVal():
+    error = cloudVal(spl, xx1, xx2, xx3, ff)
+    print('error = %g'%error)
+    assert error < 10*10*eps
+
+def test_arrayVal():
+    error = arrayVal(spl, x1, x2, x3, ff)
+    print('error = %g'%error)
+    assert error < 10*10*eps
+
+# -------------------------------------
+
+def test_pointDx():
+    error = pointDx(spl, x1, x2, x3, fx)
+    print('error = %g'%error)
+    assert error < 10*10*10*eps
+
+def test_cloudDx():
+    error = cloudDx(spl, xx1, xx2, xx3, fx)
+    print('error = %g'%error)
+    assert error < 10*10*10*eps
+
+def test_arrayDx():
+    error = arrayDx(spl, x1, x2, x3, fx)
+    print('error = %g'%error)
+    assert error < 10*10*10*eps
+
+
+def test_pointDy():
+    error = pointDy(spl, x1, x2, x3, fy)
+    print('error = %g'%error)
+    assert error < 10*10*10*eps
+
+def test_cloudDy():
+    error = cloudDy(spl, xx1, xx2, xx3, fy)
+    print('error = %g'%error)
+    assert error < 10*10*10*eps
+
+def test_arrayDy():
+    error = arrayDy(spl, x1, x2, x3, fy)
+    print('error = %g'%error)
+    assert error < 10*10*10*eps
+
+
+def test_pointDz():
+    error = pointDz(spl, x1, x2, x3, fz)
+    print('error = %g'%error)
+    assert error < 10*10*10*eps
+
+def test_cloudDz():
+    error = cloudDz(spl, xx1, xx2, xx3, fz)
+    print('error = %g'%error)
+    assert error < 10*10*10*eps
+
+def test_arrayDz():
+    error = arrayDz(spl, x1, x2, x3, fz)
+    print('error = %g'%error)
+    assert error < 10*10*10*eps
+
+# -------------------------------------
+
+def test_pointDxx():
+    error = pointDxx(spl, x1, x2, x3, fxx)
+    print('error = %g'%error)
+    assert error < 10*10*10*10*10*eps
+
+def test_cloudDxx():
+    error = cloudDxx(spl, xx1, xx2, xx3, fxx)
+    print('error = %g'%error)
+    assert error < 10*10*10*10*10*eps
+
+def test_arrayDxx():
+    error = arrayDxx(spl, x1, x2, x3, fxx)
+    print('error = %g'%error)
+    assert error < 10*10*10*10*10*eps
+
+
+def test_pointDxy():
+    error = pointDxy(spl, x1, x2, x3, fxy)
+    print('error = %g'%error)
+    assert error < 10*10*10*10*10*eps
+
+def test_cloudDxy():
+    error = cloudDxy(spl, xx1, xx2, xx3, fxy)
+    print('error = %g'%error)
+    assert error < 10*10*10*10*10*eps
+
+def test_arrayDxy():
+    error = arrayDxy(spl, x1, x2, x3, fxy)
+    print('error = %g'%error)
+    assert error < 10*10*10*10*10*eps
+
+
+def test_pointDxz():
+    error = pointDxz(spl, x1, x2, x3, fxz)
+    print('error = %g'%error)
+    assert error < 10*10*10*10*10*eps
+
+def test_cloudDxz():
+    error = cloudDxz(spl, xx1, xx2, xx3, fxz)
+    print('error = %g'%error)
+    assert error < 10*10*10*10*10*eps
+
+def test_arrayDxz():
+    error = arrayDxz(spl, x1, x2, x3, fxz)
+    print('error = %g'%error)
+    assert error < 10*10*10*10*10*eps
+
+
+def test_pointDyy():
+    error = pointDyy(spl, x1, x2, x3, fyy)
+    print('error = %g'%error)
+    assert error < 10*10*10*10*10*eps
+
+def test_cloudDyy():
+    error = cloudDyy(spl, xx1, xx2, xx3, fyy)
+    print('error = %g'%error)
+    assert error < 10*10*10*10*10*eps
+
+def test_arrayDyy():
+    error = arrayDyy(spl, x1, x2, x3, fyy)
+    print('error = %g'%error)
+    assert error < 10*10*10*10*10*eps
+
+
+def test_pointDyz():
+    error = pointDyz(spl, x1, x2, x3, fyz)
+    print('error = %g'%error)
+    assert error < 10*10*10*10*10*eps
+
+def test_cloudDyz():
+    error = cloudDyz(spl, xx1, xx2, xx3, fyz)
+    print('error = %g'%error)
+    assert error < 10*10*10*10*10*eps
+
+def test_arrayDyz():
+    error = arrayDyz(spl, x1, x2, x3, fyz)
+    print('error = %g'%error)
+    assert error < 10*10*10*10*10*eps
+
+
+def test_pointDzz():
+    error = pointDzz(spl, x1, x2, x3, fzz)
+    print('error = %g'%error)
+    assert error < 10*10*10*10*10*eps
+
+def test_cloudDzz():
+    error = cloudDzz(spl, xx1, xx2, xx3, fzz)
+    print('error = %g'%error)
+    assert error < 10*10*10*10*10*eps
+
+def test_arrayDzz():
+    error = arrayDzz(spl, x1, x2, x3, fzz)
+    print('error = %g'%error)
+    assert error < 10*10*10*10*10*eps
+
 if __name__=='__main__':
-
-    import sys
-
-    # original grid
-    n1, n2, n3 = 11, 21, 31
-    x1 = _np.linspace(0., 1., n1)
-    x2 = _np.linspace(0., 1., n2)
-    x3 = _np.linspace(0., 1., n3)
-    spl = pspline(x1, x2, x3)
-    xx1, xx2, xx3 = griddata(x1, x2, x3)
-    ff = xx1**3 + 2*xx2**3 + 3*xx3**3 + xx1**3 * xx2**2 * xx3
-    spl.setup(ff)
-
-    # new grid
-    x1 = _np.linspace(0., 1., n1-1)
-    x2 = _np.linspace(0., 1., n2-1)
-    x3 = _np.linspace(0., 1., n3-1)
-    xx1, xx2, xx3 = griddata(x1, x2, x3)
-    ff = xx1**3 + 2*xx2**3 + 3*xx3**3 + xx1**3 * xx2**2 * xx3
-    fx = 3*xx1**2 + 3*xx1**2 * xx2**2 * xx3
-    fy = 6*xx2**2 + 2*xx1**3 * xx2 * xx3
-    fz = 9*xx3**2 +   xx1**3 * xx2**2
-    fxx = 6*xx1 +  6*xx1 * xx2**2 * xx3
-    fxy = 6 * xx1**2 * xx2 * xx3
-    fyy = 12*xx2 + 2*xx1**3 * xx3
-    fxz = 3*xx1**2 * xx2**2
-    fzz = 18*xx3
-    fyz = 2*xx1**3 * xx2
-
-    cum_error = 0
 
     print('..testing interpolation....................')
 
-    error = pointVal(spl, x1, x2, x3, ff)
-    cum_error += error
-    print('error = %g'%error)
-    error = cloudVal(spl, xx1, xx2, xx3, ff)
-    cum_error += error
-    print('error = %g'%error)
-    error = arrayVal(spl, x1, x2, x3, ff)
-    cum_error += error
-    print('error = %g'%error)
+    test_pointVal()
+    test_cloudVal()
+    test_arrayVal()
 
     print('..testing 1st order derivatives............')
 
-    error = pointDx(spl, x1, x2, x3, fx)
-    cum_error += error
-    print('error = %g'%error)
-    error = cloudDx(spl, xx1, xx2, xx3, fx)
-    cum_error += error
-    print('error = %g'%error)
-    error = arrayDx(spl, x1, x2, x3, fx)
-    cum_error += error
-    print('error = %g'%error)
+    test_pointDx()
+    test_cloudDx()
+    test_arrayDx()
 
-    error = pointDy(spl, x1, x2, x3, fy)
-    cum_error += error
-    print('error = %g'%error)
-    error = cloudDy(spl, xx1, xx2, xx3, fy)
-    cum_error += error
-    print('error = %g'%error)
-    error = arrayDy(spl, x1, x2, x3, fy)
-    cum_error += error
-    print('error = %g'%error)
+    test_pointDy()
+    test_cloudDy()
+    test_arrayDy()
 
-    error = pointDz(spl, x1, x2, x3, fz)
-    cum_error += error
-    print('error = %g'%error)
-    error = cloudDz(spl, xx1, xx2, xx3, fz)
-    cum_error += error
-    print('error = %g'%error)
-    error = arrayDz(spl, x1, x2, x3, fz)
-    cum_error += error
-    print('error = %g'%error)
+    test_pointDz()
+    test_cloudDz()
+    test_arrayDz()
 
     print('..testing 2nd order derivatives............')
 
-    error = pointDxx(spl, x1, x2, x3, fxx)
-    cum_error += error
-    print('error = %g'%error)
-    error = cloudDxx(spl, xx1, xx2, xx3, fxx)
-    cum_error += error
-    print('error = %g'%error)
-    error = arrayDxx(spl, x1, x2, x3, fxx)
-    cum_error += error
-    print('error = %g'%error)
+    test_pointDxx()
+    test_cloudDxx()
+    test_arrayDxx()
 
-    error = pointDxy(spl, x1, x2, x3, fxy)
-    cum_error += error
-    print('error = %g'%error)
-    error = cloudDxy(spl, xx1, xx2, xx3, fxy)
-    cum_error += error
-    print('error = %g'%error)
-    error = arrayDxy(spl, x1, x2, x3, fxy)
-    cum_error += error
-    print('error = %g'%error)
+    test_pointDxy()
+    test_cloudDxy()
+    test_arrayDxy()
 
-    error = pointDyy(spl, x1, x2, x3, fyy)
-    cum_error += error
-    print('error = %g'%error)
-    error = cloudDyy(spl, xx1, xx2, xx3, fyy)
-    cum_error += error
-    print('error = %g'%error)
-    error = arrayDyy(spl, x1, x2, x3, fyy)
-    cum_error += error
-    print('error = %g'%error)
+    test_pointDxz()
+    test_cloudDxz()
+    test_arrayDxz()
 
-    error = pointDxz(spl, x1, x2, x3, fxz)
-    cum_error += error
-    print('error = %g'%error)
-    error = cloudDxz(spl, xx1, xx2, xx3, fxz)
-    cum_error += error
-    print('error = %g'%error)
-    error = arrayDxz(spl, x1, x2, x3, fxz)
-    cum_error += error
-    print('error = %g'%error)
+    test_pointDyy()
+    test_cloudDyy()
+    test_arrayDyy()
 
-    error = pointDyz(spl, x1, x2, x3, fyz)
-    cum_error += error
-    print('error = %g'%error)
-    error = cloudDyz(spl, xx1, xx2, xx3, fyz)
-    cum_error += error
-    print('error = %g'%error)
-    error = arrayDyz(spl, x1, x2, x3, fyz)
-    cum_error += error
-    print('error = %g'%error)
+    test_pointDyz()
+    test_cloudDyz()
+    test_arrayDyz()
 
-    error = pointDzz(spl, x1, x2, x3, fzz)
-    cum_error += error
-    print('error = %g'%error)
-    error = cloudDzz(spl, xx1, xx2, xx3, fzz)
-    cum_error += error
-    print('error = %g'%error)
-    error = arrayDzz(spl, x1, x2, x3, fzz)
-    cum_error += error
-    print('error = %g'%error)
-
-    print('cumulated error = %g in %s' % (cum_error, sys.argv[0]))
-    if cum_error > 0.30:
-       print('TEST %s FAILED' %  sys.argv[0])
+    test_pointDzz()
+    test_cloudDzz()
+    test_arrayDzz()
